08c9437f254a07f66381c3461daf0a3d94721400
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-29 17:12:55 -0400

    set up connection to mongodb via mongoose

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log(`server running on port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = "mongodb+srv://kevin:node1234@cluster0.kmmuu.mongodb.net/test";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`server running on port ${PORT}`);
+    });
+})
+.catch(err => console.log(err));
