61bd22a2ad643aedcee3940b22d90253cc88dba5
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-31 11:27:07 -0400

    add sessions and store

--- server.js
+++ server.js
@@ -4,17 +4,31 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBSession = require('connect-mongodb-session')(session);
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = "mongodb+srv://kevin:node1234@cluster0.kmmuu.mongodb.net/shopify";
 
+const store = new MongoDBSession({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+})
+
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  }));
 
 const imageRoutes = require('./routes/image.js');
 const authRoutes = require('./routes/auth.js');
@@ -30,9 +44,6 @@ mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
 })
 .catch(err => console.log(err));
 
-
-
-//sign up,
 //session,
 //upload image
 
