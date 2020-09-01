d37e607f896e67066b003cfc63de149cf4408643
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-08-31 22:30:34 -0400

    fix image path issue

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, `${file.originalname}_${Date.now()}`)
+    cb(null, `${Date.now()}_${file.originalname}`)
   }
 });
 
